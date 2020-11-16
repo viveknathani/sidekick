69cdfb26c7ce450a1b52df8ce91c3194f57d3913
viveknathani <viveknathani2402@gmail.com>
2020-11-16 15:24:39 +0530

    Test file 4.

---
+++ tests/src/grades.js
@@ -0,0 +1,48 @@
+const supertest     = require('supertest');
+const PATH          = 'http://localhost:8083/api/v1';
+const ALL           =  '/all';
+const SPECIFIC_DATA = '/test';
+const REQUEST       = supertest(PATH);
+
+function randomId() { return Math.floor(Math.random() * 20); }
+
+function getAll(id)
+{
+    describe('Get test data of this user.', function(done)
+    {
+        it('Should return 200.', function(done)
+        {
+            REQUEST
+                .get(ALL)
+                .send({ id })
+                .expect((res) => { console.log(res.body); } )
+                .end((err, res) => { if(err){ throw err;} else { done(); } });
+        });
+    });
+}
+
+function postData(data)
+{
+    describe('Post test data.', () => 
+    {
+        it('Should return 201.', function(done)
+        {
+            REQUEST
+            .post(SPECIFIC_DATA)
+            .send(data)
+            .expect(201, 'Test data added.')
+            .end((err, res) => { if(err){ throw err;} else { done(); } });
+        });
+    });
+}
+
+function start()
+{
+    const id = randomId();
+    console.log({id});
+    let data = { id: id, subject_name: 'ASD', date: '2020-10-6', max_marks: 100, scored_marks: 99};
+    postData(data);
+    getAll(id);
+}
+
+module.exports = { start: start }
