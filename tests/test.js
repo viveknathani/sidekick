e9ec1d6f03b4b64239bd7fb3430cddf790e98252
viveknathani <viveknathani2402@gmail.com>
2020-11-16 14:03:11 +0530

    Use test file 2.

--- tests/test.js
+++ tests/test.js
@@ -1,7 +1,13 @@
 const auth = require('./auth');
+const expenses = require('./expenses');
 const service = process.argv[3];
 
 if(service === 'auth')
 {
     auth.start();
+}
+
+if(service === 'expenses')
+{
+    expenses.start();
 }
