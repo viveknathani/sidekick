3c0b8e9e91e44b9736785c3f0c4951f9853b15c0
viveknathani <viveknathani2402@gmail.com>
2020-11-16 15:24:55 +0530

    Use test file 4.

--- tests/test.js
+++ tests/test.js
@@ -1,6 +1,7 @@
 const auth = require('./src/auth');
 const expenses = require('./src/expenses');
 const attendance = require('./src/attendance');
+const grades = require('./src/grades');
 const service = process.argv[3];
 
 if(service === 'src/auth')
@@ -16,4 +17,9 @@ if(service === 'src/expenses')
 if(service === 'src/attendance')
 {
     attendance.start();
+}
+
+if(service === 'src/grades')
+{
+    grades.start();
 }
