b404adbb20e438937ce2f8b8fc5e8e82e5673568
viveknathani <viveknathani2402@gmail.com>
2020-11-17 19:18:40 +0530

    Use expenses component

--- web/src/index.js
+++ web/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Auth from './components/auth/Auth';
-import './index.css';
+//import Auth from './components/auth/Auth';
+import Expenses from './components/expenses/Expenses';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Auth/>
+    <Expenses/>
   </React.StrictMode>,
   document.getElementById('root')
 );
