4df8ec6f7512e54eafac9c1e61e625e04b824012
viveknathani <viveknathani2402@gmail.com>
2020-11-17 15:13:51 +0530

    Modify design.

--- web/src/components/auth/Auth.js
+++ web/src/components/auth/Auth.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import './Auth.css';
 
+const LOGIN  = 0;
+const SIGNUP = 1;
+let selected = LOGIN;
+
 function selectLogin()
 {
+    selected = LOGIN;
     document.getElementById("login").style.backgroundColor = "#3700B3";
     document.getElementById("login").style.color = "#000000"
     document.getElementById("signup").style.backgroundColor = "#000000";
@@ -11,10 +16,17 @@ function selectLogin()
 
 function selectSignup()
 {
+    selected = SIGNUP;
     document.getElementById("signup").style.backgroundColor = "#3700B3";
     document.getElementById("signup").style.color = "#000000"
     document.getElementById("login").style.backgroundColor = "#000000";
     document.getElementById("login").style.color = "#FFFFFF";
+}
+
+function submitData()
+{
+    document.getElementById("server_response").innerText = "Please wait...";
+    document.getElementById("server_response").style.color = "white";
 }
 
 class Auth extends React.Component
@@ -34,10 +46,10 @@ class Auth extends React.Component
                             <input placeholder="email" ></input>
                             <input placeholder="password" type="password"></input>
                         </form>
+                        <p id="server_response"></p>
                     </div>
-
                     <div id="bottom">
-                        <button id="submit">submit</button>
+                        <button id="submit" onClick={submitData}>submit</button>
                     </div>
                 </div>
             </div>
