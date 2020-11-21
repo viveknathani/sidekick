affdcb2f5970bbd05a56122be76b1b61cfc62899
viveknathani <viveknathani2402@gmail.com>
2020-11-21 19:57:43 +0530

    Conditional render

--- web/src/components/App.js
+++ web/src/components/App.js
@@ -4,13 +4,56 @@ import Expenses from './expenses/Expenses';
 import Attend from './attendance/Attend';
 import Grades from './grades/Grades';
 import Welcome from './welcome/Welcome';
+import Auth from './auth/Auth';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 class App extends React.Component
 {
+    constructor(props)
+    {
+        super(props);
+        this.state = {
+            id : -1
+        }
+        this.isAuthenticated = this.isAuthenticated.bind(this);
+        this.parentUpdate = this.parentUpdate.bind(this);
+    }
+
+    parentUpdate()
+    {
+        this.forceUpdate();
+    }
+
+    async isAuthenticated()
+    {
+       try 
+       {
+           let token = localStorage.getItem("token");
+           let response = await fetch('/auth/verifyToken', {
+               method: 'POST',
+               body:JSON.stringify({token})
+           });
+
+           if(response.id) {
+               this.setState({id: parseInt(response.id)});
+               return true;
+           }
+           else
+           {
+               return false;
+           }
+       }
+
+       catch(err)
+       {
+           console.log(err);
+           return false;
+       }
+    }
+
     render()
     {
-        return(
+        let content = this.isAuthenticated ? (<Auth parentUpdate={this.parentUpdate}/>) : (
             <Router>
                 <div className="App">
                     <Header/>
@@ -22,7 +65,8 @@ class App extends React.Component
                     </Switch>
                 </div>
             </Router>
-        );
+        )
+        return content;
     }
 };
 
