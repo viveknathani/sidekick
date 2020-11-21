14718988d3110e88df3a7d6d09f9759d4a5551bb
viveknathani <viveknathani2402@gmail.com>
2020-11-21 18:38:38 +0530

    Fix order of express.json() call

--- api-gateway/index.js
+++ api-gateway/index.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const express    = require('express');
 const paths      = require('./paths/entry');
 const app        = express();
-app.use(express.json());
 
 paths(app);
+app.use(express.json());
 app.listen(process.env.PORT, () => console.log('Gateway up and running.'));
